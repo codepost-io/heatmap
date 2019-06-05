98e4c5bd4e2a96b33576abfa27105dcd1f5f70a2
Jérémie Lumbroso <lumbroso@cs.princeton.edu>
2019-06-04 20:36:13 -0400

    Fixed __init__.py to include draw

--- heatmap/__init__.py
+++ heatmap/__init__.py
@@ -1,2 +1,10 @@
 
+from . import util
+
+_logger = util.getLogger()
+
+_logger.debug("Pkg loading: Loading 'preprocess'...")
 from . import preprocess
+
+_logger.debug("Pkg loading: Loading 'draw'...")
+from . import draw
